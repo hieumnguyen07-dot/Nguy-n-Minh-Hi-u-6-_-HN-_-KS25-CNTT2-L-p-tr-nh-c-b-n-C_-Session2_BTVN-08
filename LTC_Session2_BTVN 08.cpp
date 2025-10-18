bc564da5bb68301d9579a2b505aa83c368dbcf35
HIEU 6 <hieu.m.nguyen07@gmail.com>
2025-10-18 16:18:31 +0700

    Simplify number reversal logic in main function
    Refactor code to reverse digits of a number more efficiently.

--- LTC_Session2_BTVN 08.cpp
+++ LTC_Session2_BTVN 08.cpp
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    int number = 12345;
-    int reverse;
-    int d1, d2, d3, d4, d5; 
-    d1 = number % 10;        
-    number = number / 10;    
-    number = number / 10;    
-    d4 = number % 10;        
-    number = number / 10;   
-    d5 = number % 10;        
-    reverse = d1 * 10000 + d2 * 1000 + d3 * 100 + d4 * 10 + d5;
-
-    printf("So dao nguoc la: %d\n", reverse);
-
-    return 0;
+int main(){
+	int number = 12345 ;
+	int d1 = number % 10 ;
+	int d2 = (number / 10) % 10 ;
+	int d3 = (number / 100) % 10 ;
+	int d4 = (number / 1000) % 10 ;
+	int d5 = (number / 10000) % 10 ;
+	
+	printf("%d%d%d%d\n" , d2, d3, d4, d5) ;
+	
+	return 0 ;
 }
